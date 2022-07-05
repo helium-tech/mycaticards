81fc52fbe91aec73e4f02eb4d817ce2933e1eb7c
jagbegbo@gmail.com <jagbegbo@gmail.com>
2022-07-05 10:45:35 +0000

    add linkPage

---
+++ pages/linkPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="flex flex-col items-center justify-center w-full h-auto p-6 bg-gradient-to-b from-purple-600 to-pink-500">
+          <img src="@/assets/images/avatar.jpg" alt="" class="m-auto rounded-full" height="40%" width="40%">
+    <div class="flex flex-col justify-center my-5 text-xl font-semibold text-center text-white">
+        <h3 class="">HubSpot Blogs</h3>
+        <h4 class="text-base">Select one of the links below to visit a HubSpot Blog</h4>
+    </div>
+    <div class="flex flex-col w-auto h-auto my-6 space-y-4">
+        <a href="#" class="w-full h-auto p-4 text-xl font-semibold text-center text-white bg-transparent border-2 border-white hover:bg-white hover:text-purple-600">HubSpot Marketing Blog</a>
+        <a href="#" class="w-full h-auto p-4 text-xl font-semibold text-center text-white bg-transparent border-2 border-white hover:bg-white hover:text-purple-600">HubSpot Website Blog</a>
+        <a href="#" class="w-full h-auto p-4 text-xl font-semibold text-center text-white bg-transparent border-2 border-white hover:bg-white hover:text-purple-600">HubSpot Sales Blog</a>
+        <a href="#" class="w-full h-auto p-4 text-xl font-semibold text-center text-white bg-transparent border-2 border-white hover:bg-white hover:text-purple-600">HubSpot Service Blog</a>
+    </div>
+    
+    <img src="@/assets/images/logo-removebg-preview.png" alt=""  width="150" class="">
+    </div>
+</template>
