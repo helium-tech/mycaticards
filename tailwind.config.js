d3366078a30a0cd9d06591d6dd11aea42b3da1f0
jagbegbo@gmail.com <jagbegbo@gmail.com>
2022-07-05 10:38:43 +0000

    Profil update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+    content: [
+      "./public/**.html"
+    ],
     theme: {
-        extends: {
-            fontFamily: {
-                'sans': ['Montserrat']
-            }
+      borderRadius:{
+        'xxl':'40px',
+        'xl':'15px',
+        'full':'100%'
+      },
+      extend: {
+        fontFamily:{
+          'police':['']
         }
-    }
-}
+      },
+    },
+    plugins: [],
+  }
+  
